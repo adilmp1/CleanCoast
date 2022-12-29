ebd1494a919b100fcf0734d37b84d566f696fc73
Abhinav <abhinavmohanan17@gmail.com>
2022-12-29 14:19:34 +0530

    Homepage#2

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div>
+        <div>
+            <div>Heading</div>
+            <div>Description</div>
+        </div>
+        <div>
+            <div>Heading</div>
+            <div>Description</div>
+        </div>
+        
+    </div>
+  )
+}
